b36682ca19ebaa866ebf3ef1d230b187636296cd
pablajsl <kanin.pavel+pablajsl@gmail.com>
2021-02-14 10:50:06 +0700

    Add set x

--- docs/_plugins/search_index.rb
+++ docs/_plugins/search_index.rb
@@ -1,6 +1,11 @@
 require 'elasticsearch'
 
-posts = File.open("./posts.txt").each_line.to_a.map(&:chomp).map { |v| File.basename(v) }
+posts = File.open("./changes.txt") \
+  .each_line \
+  .to_a \
+  .map(&:chomp) \
+  .select { |v| v.start_with?("docs/_posts") } \
+  .map { |v| File.basename(v) }
 Jekyll::Hooks.register :posts, :post_render do |post|
   next unless posts.include? post.basename
   puts "Indexing #{post.url}..."
